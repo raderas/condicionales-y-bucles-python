36f0f9f7a1a1acf32dfc6151e8cb2abcabe0b331
Rafael Deras <mr_musica@outlook.com>
2026-05-28 10:34:05 -0600

    Delete ejemplo-if1.py

--- ejemplo-if1.py
+++
@@ -1,6 +0,0 @@
-numero = input("Introduzca la nota del alumno (0-100): ")
-
-if float(numero) > 60:
-    print("El alumno ha aprobado la asignatura =)")
-else:
-    print("El alumno ha reprobado la asignatura =(")
